616bd45c65498247eb89e6fe48c5d03a61585fce
Naman <contact.nmn@gmail.com>
2018-10-16 00:08:57 +0530

    mongoose

---
+++ server/server.js
@@ -0,0 +1,62 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+// var otherTodo = new Todo({
+//   text: 'Feed the cat',
+//   completed: true,
+//   completedAt: 123
+// });
+//
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+var user = new User({
+  email: 'naman@example.com'
+});
+
+user.save().then((doc) => {
+  console.log('User saved', doc)
+}, (e) => {
+  console.log('Unable to save user', e);
+});
